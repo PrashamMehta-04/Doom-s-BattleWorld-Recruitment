831644e64e16462edf212ab276b7068fd0f5fce9
Daredevil124 <devarsh.d2@ahduni.edu.in>
2025-06-28 16:31:12 +0530

    Login-Button

---
+++ client/src/Components/Login_Button.jsx
@@ -0,0 +1,9 @@
+import '../Components_CSS/Login_Buttonc.css'
+const Login_Button=({text})=>{
+    return(
+        <div>
+            <button className='but1'>{text}</button> 
+        </div>
+    )
+} 
+export default Login_Button;
---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import Login_Button from '../Components/Login_Button'
+const Home=()=>{
+    return (
+        <div>
+            <Login_Button text="Login as Marvel Hero"/><br/>
+            <Login_Button text="Login as Doom"/>
+        </div>
+    )
+} 
+export default Home; 

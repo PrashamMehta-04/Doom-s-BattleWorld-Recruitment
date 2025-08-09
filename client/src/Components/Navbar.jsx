66895d74b1d23b18f7c0f526f2f17f063f616782
Prasham Mehta <prashammehta360@gmail.com>
2025-08-09 22:11:24 +0530

    navbar_change

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../Components_CSS/Navbarc.css';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -9,19 +10,18 @@ const Navbar = () => {
     <header className="custom-navbar">
       <div className="custom-logo-section">
         <span className="custom-logo-icon">⚡</span>
-        <span 
-          className="custom-logo-text" 
-          onClick={() => navigate('/')} 
+        <span
+          className="custom-logo-text"
+          onClick={() => navigate('/')}
           style={{ cursor: 'pointer' }}
         >
           BattleWorld
         </span>
       </div>
-
       <nav className="custom-nav-links">
-        <a onClick={() => navigate('/about')} href="">About</a>
-        <a onClick={() => navigate('/Privacy-Terms')} href="">Privacy/Terms</a>
-        <a onClick={() => navigate('/contact')} href="">Contact Us</a>
+        <Link to="/about">About</Link>
+        <Link to="/Privacy-Terms">Privacy/Terms</Link>
+        <Link to="/contact">Contact Us</Link>
       </nav>
     </header>
   );

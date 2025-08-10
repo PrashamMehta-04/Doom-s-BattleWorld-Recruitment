f98e7c327b59fd669105aa5f5ac49b74cb1ee484
Daredevil124 <devarsh.d2@ahduni.edu.in>
2025-08-10 10:49:13 +0530

    usnique email

--- server/index.js
+++ server/index.js
@@ -88,7 +88,8 @@ app.post('/api/login', async(req,res)=>{
 app.post('/api/signup',async(req,res)=>{
     const{username,password,name,type,Email}=req.body;
     try{
-        const check=await user.findOne({username});
+        const check=await user.findOne({Username:username});
+        const check2=await user.findOne({email:Email})
         if(check){
             res.status(401).send("Username already exists");
         }
@@ -303,7 +304,7 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'https://doom-s-battleworld-recruitment.vercel.app'],
+    origin:process.env.CLIENT_URL,
     credentials: true
   },
 });
